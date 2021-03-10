f47609fa94201ef7ca6473b969828297b75e80c8
sergey <azazela60@yandex.ru>
2021-03-10 07:55:27 +0300

    ДЗ 5

---
+++ src/test/java/ru/geekbrains/TestLogin.java
@@ -0,0 +1,98 @@
+package ru.geekbrains;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+class TestLogin {
+
+
+    private final String LOGIN_PAGE_URL = "https://www.diary.ru";
+    private final String LOGIN = "nbfgdc45";
+    private final String PASSWORD = "aaa#a4890";
+    private WebDriver driver;
+    private JavascriptExecutor jsExecutor;
+
+
+    public TestLogin(WebDriver driver, JavascriptExecutor jsExecutor) {
+        this.driver = driver;
+        this.jsExecutor = jsExecutor;
+    }
+
+    @BeforeAll
+    public static void setupWebDriverManager() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+
+    @BeforeEach
+    public void beforeTest() {
+        setUpDriverSession();
+        login();
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void createNewExpenseTest() {
+
+        By btnLocator = By.xpath(
+                ".//[@class='nav navbar-nav navbar-index'/li[@class='dropdown open']/a[@class='dropdown-toggle']/span[text()='Вход']");
+
+        Actions actions = new Actions(driver);
+        WebElement menu = driver.findElement(btnLocator);
+        Actions actions1 = actions.moveToElement(menu);
+        actions.click().build().perform();
+
+    }
+    @Test
+    public void testLogin(){
+        login();
+    }
+
+        private void login () {
+            driver.get(LOGIN_PAGE_URL);
+
+
+            WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"drop-login\"]"));
+            loginButton.click();
+            WebElement loginTextInput = driver.findElement(By.cssSelector("input[id='usrlog2']"));
+            loginTextInput.sendKeys(LOGIN);
+
+            WebElement passwordTextInput = driver.findElement(By.cssSelector("input[id='usrpass2']"));
+            passwordTextInput.sendKeys(PASSWORD);
+
+            jsExecutor.executeScript("window.alert('I love Web Driver!')");
+
+            WebElement LoginButton1 = driver.findElement(By.xpath("//*[@id=\"signin_btn\"]"));
+
+            Actions loginButton1 = null;
+            loginButton1.click();
+
+        }
+
+        private void setUpDriverSession () {
+            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            jsExecutor = (JavascriptExecutor) driver;
+        }
+
+    }
+
+
+
